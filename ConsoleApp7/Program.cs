085119123ed27f122e95fad282870b8ab8b129b1
Jamil-Arifoglu <jamilai@code.edu.az>
2022-11-14 06:29:02 +0400

    Task

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region Verilen eded 70-den boyukdurse, 70-le arasindaki ferqin 3 qatini ekrana yazdiran
+//string numStr = Console.ReadLine();
+
+//int num=Convert.ToInt32(numStr);
+//Console.WriteLine("Answer:"+ num);
+
+//if(num>70)
+//{
+//    num=3 * (num - 70);
+//    Console.WriteLine(num);
+
+
+//}
+//else
+//{
+//    num -= 70;
+//    Console.WriteLine(num);
+//} 
+#endregion
+
+#region X, Y və Z . Hansı ədədin böyük olduğunu ekrana çıxardan bir alqoritm yazırsız.
+//int x = 5;
+//int y = 7;
+//int z = 10;
+//if (x > y && x > z)
+//{
+//    Console.WriteLine(x);
+//}
+//else if(y>x && y > z)
+//{
+//    Console.WriteLine(y);
+
+//}
+//else
+//{
+//    Console.WriteLine(z);
+//} 
+#endregion
+
+#region Verilmiş n və m (n<m) ədədləri arasında neçə tək ədəd olduğunu tapın.
+//int n = 5;
+//int m = 12;
+//int result = 2;
+//for (int i = n; i < m; i++)
+//{
+//    result *= i;
+//    Console.WriteLine(i);
+//} 
+#endregion
+
+#region Verilen 2 eded arasindaki ededleri ekrana yazdiran alqoritm
+//string numStr = Console.ReadLine();
+//int num=Convert.ToInt32(numStr);
+//Console.WriteLine("Answer:" + num);
+
+//for (int i =0; i < num; i++)
+//{
+//    Console.WriteLine(i);
+//} 
+#endregion
+
+#region  Ədədin 2-nin qüvvəti olub olmadığı tapan alqoritm
+//string numStr =Console.ReadLine();
+//int num= Convert.ToInt32(numStr);
+//Console.WriteLine("Answer:"+num);
+
+//  if (num % 2 == 0)
+
+//    {
+//       num /= 2;
+//    Console.WriteLine("quvvetidir");
+//}
+//else
+//{
+
+//    Console.WriteLine("quvveti deyil");
+//} 
+#endregion
+
